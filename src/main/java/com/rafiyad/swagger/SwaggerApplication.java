30736c0c5ae8a065cb39dc65d5af18ad739e3115
Rafiyad <rafiyad.celloscope@gmail.com>
2025-07-28 15:43:00 +0600

    yaml file added to the project folder

--- src/main/java/com/rafiyad/swagger/SwaggerApplication.java
+++ src/main/java/com/rafiyad/swagger/SwaggerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Product API", version = "1.0", description = "API for managing products"))
+//Top view on swagger ui page
+@OpenAPIDefinition(info = @Info(title = "Product API", version = "3.0", description = "API for managing products"))
+
 public class SwaggerApplication {
 
     public static void main(String[] args) {
